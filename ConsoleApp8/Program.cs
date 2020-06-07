c7143823461285664c195723c43ec65e80496165
T-Ngt <surfingcyclone123@gmail.com>
2020-06-07 16:41:10 +0900

    teest

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -5,9 +5,8 @@ namespace ConsoleApp8
     class Program
     {
         static void Main(string[] args)
-        {  
-
-            Console.WriteLine("Hello World!");
+        {
+            Console.WriteLine("Hello World! Test");
         }
     }
 }
